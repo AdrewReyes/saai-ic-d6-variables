77a8a13ec2e0d6768e22164cf939cee1e8fa4bd9
AdrewReyes <andrewreyes.m@gmail.com>
2024-11-07 03:34:44 +0000

    Learnign JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log ("Excited to Start JavaScript!")
+console.log ("Hello")
+console.log ("animal")
+const myFavoriteAnimal = "dog"
+console.log (myFavoriteAnimal)
+const myName = "Daewu"
+console.log (myName)
+
+let favoriteFood = "tacos"
+console.log (favoriteFood)
+
+favoriteFood = "epic wings"
+console.log (favoriteFood)
+
+const favoriteHoliday = "Christmas"
+console.log (favoriteHoliday)
+
+console.log ("value of favorite food" , favoriteFood )
